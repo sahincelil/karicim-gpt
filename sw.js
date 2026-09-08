f594d9faa29f4e23b5f90e85fa1f151051a657a2
sahincelil <100981854+sahincelil@users.noreply.github.com>
2026-09-08 22:12:01 +0300

    fix: refresh offline shell cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='karicimgpt-shell-v1';
-const SHELL=['/','/index.html','/app.js','/manifest.webmanifest'];
+const CACHE='karicimgpt-shell-v2';
+const SHELL=['/','/index.html','/app.js','/manifest.webmanifest','/icon-192.svg','/icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
